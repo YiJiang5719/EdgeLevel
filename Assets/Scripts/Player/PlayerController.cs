011037679b0b9bcd5443d594458bf4785b4eaef1
Prinny <601239484@qq.com>
2020-06-08 14:10:28 +0800

    # PlayerController

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float xSpeed;
+    public float ySpeed;
+    public float zSpeed;
+
+    [HideInInspector]
+    public float currentZSpeed;
+
+    Rigidbody rig;
+    Animator anim;
+    UIJoystick joy;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        joy = FindObjectOfType<UIJoystick>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig.velocity = new Vector3(0, 0, zSpeed);
+        currentZSpeed = zSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.velocity = new Vector3(joy.Horizontal * xSpeed, joy.Vertical * ySpeed, currentZSpeed);
+    }
+
+    public void ZSpeedUp()
+    {
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("SpeedUp"))
+            anim.SetTrigger("SpeedUp");
+    }
+}
---
+++ Assets/Scripts/UI/UIJoystick.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class UIJoystick : Joystick
+{
+    public event Action OnMouseDownEvent;
+    public event Action OnMouseUpEvent;
+    public float MoveThreshold { get { return moveThreshold; } set { moveThreshold = Mathf.Abs(value); } }
+
+    [SerializeField] private float moveThreshold = 0;
+    [SerializeField] private JoystickType joystickType = JoystickType.Fixed;
+
+    private Vector2 fixedPosition = Vector2.zero;
+
+    public void SetMode(JoystickType joystickType)
+    {
+        this.joystickType = joystickType;
+        if (joystickType == JoystickType.Fixed)
+        {
+            background.anchoredPosition = fixedPosition;
+            background.gameObject.SetActive(true);
+        }
+        else
+            background.gameObject.SetActive(false);
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        fixedPosition = background.anchoredPosition;
+        SetMode(joystickType);
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        if (joystickType != JoystickType.Fixed)
+        {
+            background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
+            background.gameObject.SetActive(true);
+        }
+        if (OnMouseDownEvent != null)
+            OnMouseDownEvent();
+        base.OnPointerDown(eventData);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        if (joystickType != JoystickType.Fixed)
+            background.gameObject.SetActive(false);
+
+        if (OnMouseUpEvent != null)
+            OnMouseUpEvent();
+        base.OnPointerUp(eventData);
+    }
+
+    protected override void HandleInput(float magnitude, Vector2 normalised, Vector2 radius, Camera cam)
+    {
+        if (joystickType == JoystickType.Dynamic && magnitude > moveThreshold)
+        {
+            Vector2 difference = normalised * (magnitude - moveThreshold) * radius;
+            background.anchoredPosition += difference;
+        }
+        base.HandleInput(magnitude, normalised, radius, cam);
+    }
+}
+
