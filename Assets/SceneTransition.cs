b42718553cbce8330813de1d68698df69273fa73
Yijiang <Yijiang5719@gmail.com>
2020-06-20 17:41:29 +0800

    场景串联

---
+++ Assets/SceneTransition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public string SceneName;
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && other.isTrigger)
+        {
+            Debug.Log("SceneLoad!");
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+}
